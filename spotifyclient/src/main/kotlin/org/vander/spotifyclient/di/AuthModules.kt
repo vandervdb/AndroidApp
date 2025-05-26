5052f9c73c838faec286bc262b2cc9ca903f3cdf
Arnaud  Vanderbecq <arnaud.vander@proton.me>
2025-05-26 15:08:57 +0200

    Move session logic from screen to app level to prevent lifecycle issues - Add animatn pon truncated track title

--- spotifyclient/src/main/kotlin/org/vander/spotifyclient/di/AuthModules.kt
+++ spotifyclient/src/main/kotlin/org/vander/spotifyclient/di/AuthModules.kt
@@ -3,19 +3,17 @@ package org.vander.spotifyclient.di
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.components.SingletonComponent
 import org.vander.spotifyclient.data.auth.SpotifyAuthClient
 import org.vander.spotifyclient.domain.auth.ISpotifyAuthClient
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class AuthModule {
 
     @Binds
-    @ViewModelScoped
     abstract fun bindSpotifyAuthClient(
-        spotifyAuthClient: SpotifyAuthClient
+        impl: SpotifyAuthClient
     ): ISpotifyAuthClient
 }
 
